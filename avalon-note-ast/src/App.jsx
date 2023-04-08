414403dde7de2688d4fe97e0a1032ee683dfa5b0
Yi Liu <31673018+o0ez0o@users.noreply.github.com>
2023-04-08 04:42:16 -0700

    Fixed Responsive Designs

--- avalon-note-ast/src/App.jsx
+++ avalon-note-ast/src/App.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import RoundNote from "./Components/RoundNote/RoundNote.jsx"
-import QuestPanel from './Components/QuestPanel/QuestPanel.jsx';
-import Header from './Components/Header/Header.jsx';
-
-
+import React from "react";
+import RoundNote from "./Components/RoundNote/RoundNote.jsx";
+import QuestPanel from "./Components/QuestPanel/QuestPanel.jsx";
+import Header from "./Components/Header/Header.jsx";
 
 export function App(props) {
-  const clipNames = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10'];
-  const statusNames = ['FAILED', 'SUCCEED', 'FAILED x2', 'FAILED x3', 'FAILED x4'];
+  const clipNames = [
+    "P1",
+    "P2",
+    "P3",
+    "P4",
+    "P5",
+    "P6",
+    "P7",
+    "P8",
+    "P9",
+    "P10",
+  ];
+  const statusNames = [
+    "FAILED",
+    "SUCCEED",
+    "FAILED x2",
+    "FAILED x3",
+    "FAILED x4",
+  ];
   const onSubmit = (data) => {
-    console.log (data);
-    console.log (data.selectedPlayerID, data.agreedPlayerID, data.questResult)
-  }
-  
+    console.log(data);
+    console.log(data.selectedPlayerID, data.agreedPlayerID, data.questResult);
+  };
 
   // return (
   //   <div>
@@ -22,15 +36,21 @@ export function App(props) {
   //   </div>
   // );
   return (
-    <div style={{display: 'flex', flexDirection: "row", gap: '36px', width: 'wrap'}}>
-      <div style={{flex: 1}}>
-        <Header/>
-        <QuestPanel/>
-      </div>  
-      <RoundNote onSubmit = {onSubmit}></RoundNote>
-      
-
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "36px",
+        width: "wrap",
+      }}
+    >
+      <div style={{ flex: 1 }}>
+        <Header />
+        <QuestPanel />
+      </div>
+      <RoundNote onSubmit={onSubmit}></RoundNote>
+      <div style={{ width: "320px" }}></div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
